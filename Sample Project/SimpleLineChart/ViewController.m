a5616898e9633ee78b90717c884de8da1dcd5538
Petr Korolev <sky4winder@gmail.com>
2014-11-04 12:20:59 +0200

    remove duplicate initialization + move values generation in separate method.

--- Sample Project/SimpleLineChart/ViewController.m
+++ Sample Project/SimpleLineChart/ViewController.m
@@ -30,14 +30,7 @@
     
     previousStepperValue = self.graphObjectIncrement.value;
     totalNumber = 0;
-    
-    for (int i = 0; i < 9; i++) {
-        [self.arrayOfValues addObject:@([self getRandomInteger])]; // Random values for the graph
-        [self.arrayOfDates addObject:[NSString stringWithFormat:@"%@", @(2000 + i)]]; // Dates for the X-Axis of the graph
-        
-        totalNumber = totalNumber + [self.arrayOfValues[(NSUInteger) i] intValue]; // All of the values added together
-    }
-    
+
     /* This is commented out because the graph is created in the interface with this sample app. However, the code remains as an example for creating the graph using code.
      BEMSimpleLineGraphView *myGraph = [[BEMSimpleLineGraphView alloc] initWithFrame:CGRectMake(0, 60, 320, 250)];
      myGraph.delegate = self;
@@ -45,54 +38,42 @@
      [self.view addSubview:myGraph]; */
     
     // Customization of the graph
-    self.myGraph.colorTop = [UIColor colorWithRed:31.0f/255.0f green:187.0f/255.0f blue:166.0f/255.0f alpha:1.0];
-    self.myGraph.colorBottom = [UIColor colorWithRed:31.0f/255.0f green:187.0f/255.0f blue:166.0f/255.0f alpha:1.0];
     self.myGraph.colorLine = [UIColor whiteColor];
     self.myGraph.colorXaxisLabel = [UIColor whiteColor];
     self.myGraph.colorYaxisLabel = [UIColor whiteColor];
     self.myGraph.widthLine = 3.0;
     self.myGraph.enableTouchReport = YES;
     self.myGraph.enablePopUpReport = YES;
-    self.myGraph.enableBezierCurve = YES;
     self.myGraph.enableYAxisLabel = YES;
     self.myGraph.autoScaleYAxis = YES;
     self.myGraph.alwaysDisplayDots = NO;
     self.myGraph.enableReferenceXAxisLines = YES;
     self.myGraph.enableReferenceYAxisLines = YES;
     self.myGraph.enableReferenceAxisFrame = YES;
-    self.myGraph.animationGraphStyle = BEMLineAnimationDraw;
-    
+
     //setup initial selectet segment
     self.curveChoice.selectedSegmentIndex = self.myGraph.enableBezierCurve;
 
     // The labels to report the values of the graph when the user touches it
     self.labelValues.text = [NSString stringWithFormat:@"%i", [[self.myGraph calculatePointValueSum] intValue]];
     self.labelDates.text = @"between 2000 and 2010";
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self refresh:nil];
 }
 
 #pragma mark - Graph Actions
 
 - (IBAction)refresh:(id)sender {
-    [self.arrayOfValues removeAllObjects];
-    [self.arrayOfDates removeAllObjects];
-    
-    for (int i = 0; i < self.graphObjectIncrement.value; i++) {
-        [self.arrayOfValues addObject:@([self getRandomInteger])]; // Random values for the graph
-        [self.arrayOfDates addObject:[NSString stringWithFormat:@"%@", @(2000 + i)]]; // Dates for the X-Axis of the graph
-        
-        totalNumber = totalNumber + [(self.arrayOfValues)[i] intValue]; // All of the values added together
-    }
+
+    [self fillArrayWithRandomValues];
+
+    self.myGraph.enableBezierCurve = (BOOL) self.curveChoice.selectedSegmentIndex;
+
     UIColor *color;
     if (self.graphColorChoice.selectedSegmentIndex == 0) color = [UIColor colorWithRed:31.0f/255.0f green:187.0f/255.0f blue:166.0f/255.0f alpha:1.0];
     else if (self.graphColorChoice.selectedSegmentIndex == 1) color = [UIColor colorWithRed:255.0f/255.0f green:187.0f/255.0f blue:31.0f/255.0f alpha:1.0f];
     else if (self.graphColorChoice.selectedSegmentIndex == 2) color = [UIColor colorWithRed:0.0f green:140.0f/255.0f blue:255.0f/255.0f alpha:1.0];
-    
-    self.myGraph.enableBezierCurve = (BOOL) self.curveChoice.selectedSegmentIndex;
+
     self.myGraph.colorTop = color;
     self.myGraph.colorBottom = color;
     self.myGraph.backgroundColor = color;
@@ -100,8 +81,22 @@
     self.labelValues.textColor = color;
     self.navigationController.navigationBar.tintColor = color;
     
-    self.myGraph.animationGraphStyle = BEMLineAnimationFade;
+    self.myGraph.animationGraphStyle = BEMLineAnimationDraw;
+
     [self.myGraph reloadGraph];
+}
+
+- (void)fillArrayWithRandomValues
+{
+    [self.arrayOfValues removeAllObjects];
+    [self.arrayOfDates removeAllObjects];
+
+    for (int i = 0; i < self.graphObjectIncrement.value; i++) {
+        [self.arrayOfValues addObject:@([self getRandomInteger])]; // Random values for the graph
+        [self.arrayOfDates addObject:[NSString stringWithFormat:@"%@", @(2000 + i)]]; // Dates for the X-Axis of the graph
+
+        totalNumber = totalNumber + [(self.arrayOfValues)[i] intValue]; // All of the values added together
+    }
 }
 
 - (NSInteger)getRandomInteger
